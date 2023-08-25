74f102af770fbeee6f046f9a1a14894d61dcee6c
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-08-25 23:11:09 +0200

    feat: create communication interface

---
+++ TyphoonHilApi/Communication/ICommunication.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TyphoonHilApi.Communication
+{
+    internal interface ICommunication
+    {
+        void Request(string operation, JObject parameters);
+        JObject Response();
+    }
+}
