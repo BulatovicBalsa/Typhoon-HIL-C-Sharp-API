3c10d03cdd58b0bf194372430b2151dbbe590812
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-08-26 00:15:04 +0200

    feat: Create SchematicApi class

---
+++ TyphoonHilApi/Communication/APIs/SchematicAPI.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TyphoonHilApi.Communication.APIs
+{
+    internal class SchematicAPI : AbsractAPI
+    {
+        public override int ProperPort => _schematicApiPort;
+
+        public JObject Load(string filename)
+        {
+            return Request("load", new JObject { "filename", filename });
+        }
+
+        public JObject Compile()
+        {
+            return Request("compile", new());
+        }
+    }
+}
