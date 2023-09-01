c14e2f9228337dfb4eb55cfae7006b9c6d71db5a
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-09-01 11:00:13 +0200

    feat: Create HilAPI class

---
+++ TyphoonHilApi/Communication/APIs/HilAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TyphoonHilApi.Communication.APIs
+{
+    internal class HilAPI : AbsractAPI
+    {
+        public override int ProperPort => Ports.HilApiPort;
+        public HilAPI() { }
+
+        public HilAPI(ICommunication communication):base(communication) { }
+
+
+    }
+}
