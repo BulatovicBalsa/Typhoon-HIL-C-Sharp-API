42ff26a1a35dd9d555b0bef38a3ec558b455dc2b
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-09-01 16:48:40 +0200

    feat: Create PvGeneratorAPI class

---
+++ TyphoonHilApi/Communication/Exceptions/PvGeneratorAPIException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace TyphoonHilApi.Communication.Exceptions
+{
+    [Serializable]
+    internal class PvGeneratorAPIException : Exception
+    {
+        public PvGeneratorAPIException()
+        {
+        }
+
+        public PvGeneratorAPIException(string? message) : base(message)
+        {
+        }
+
+        public PvGeneratorAPIException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected PvGeneratorAPIException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
