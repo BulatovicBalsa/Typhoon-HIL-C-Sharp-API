f36a955d35a0799c404ac4579f09961d3b1d1e7b
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-09-03 23:55:30 +0200

    feat: Create FirmwareManagerAPI class

---
+++ TyphoonHilApi/Communication/APIs/FirmwareManagerAPIException.cs
@@ -0,0 +1,12 @@
+﻿namespace TyphoonHilApi.Communication.APIs;
+
+public class FirmwareManagerAPIException : Exception
+{
+    public FirmwareManagerAPIException(string message) : base(message)
+    {
+    }
+
+    public FirmwareManagerAPIException() : base()
+    {
+    }
+}
---
+++ TyphoonHilApi/Communication/Exceptions/FirmwareManagerAPI.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using TyphoonHilApi.Communication.APIs;
+
+namespace TyphoonHilApi.Communication.Exceptions;
+
+public class FirmwareManagerAPI : AbsractAPI
+{
+    public override int ProperPort => Ports.FwApiPort;
+
+    protected override JObject HandleRequest(string method, JObject parameters)
+    {
+        var res = Request(method, parameters);
+        if (!res.ContainsKey("error")) return res;
+        var msg = (string)res["error"]!["message"]!;
+        throw new FirmwareManagerAPIException(msg);
+    }
+
+    public JObject GetHilInfo()
+    {
+        return (JObject)HandleRequest("get_hil_info")["result"]!;
+    }
+}
